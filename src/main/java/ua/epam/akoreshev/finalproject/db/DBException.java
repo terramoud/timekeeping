669d90bbc5c7fe347baf7bc5a4f5860cc06a1f0d
Alexander Koreshev <a.koreshev1@gmail.com>
2022-07-29 20:37:46 +0300

    Add javadoc to DBException class

--- src/main/java/ua/epam/akoreshev/finalproject/db/DBException.java
+++ src/main/java/ua/epam/akoreshev/finalproject/db/DBException.java
@@ -1,5 +1,11 @@
 package ua.epam.akoreshev.finalproject.db;
 
+/**
+ * Throws an exception that
+ * occurs while working with the database
+ *
+ * @author A.Koreshev
+ */
 public class DBException extends Exception {
     public DBException(String message, Throwable cause) {
         super(message, cause);
