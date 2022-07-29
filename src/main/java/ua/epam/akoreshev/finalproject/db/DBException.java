96a913873789b286f085dc5a3326f2a17f22974a
Alexander Koreshev <a.koreshev1@gmail.com>
2022-07-29 17:52:44 +0300

    Create DBException class

---
+++ src/main/java/ua/epam/akoreshev/finalproject/db/DBException.java
@@ -0,0 +1,7 @@
+package ua.epam.akoreshev.finalproject.db;
+
+public class DBException extends Exception {
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
