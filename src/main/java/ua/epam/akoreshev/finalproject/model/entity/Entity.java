23d941d196580c403171a56ed0a27c03ca660bcd
Alexander Koreshev <a.koreshev1@gmail.com>
2022-07-30 16:17:52 +0300

    Create base abstract Entity class

---
+++ src/main/java/ua/epam/akoreshev/finalproject/model/entity/Entity.java
@@ -0,0 +1,30 @@
+package ua.epam.akoreshev.finalproject.model.entity;
+
+/**
+ * Basic class for every model entity
+ */
+public abstract class Entity {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+}
