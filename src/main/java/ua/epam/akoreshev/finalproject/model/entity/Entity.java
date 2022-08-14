1d12f870cdcb5f976fce390b41eeff1cb6bd7432
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-14 18:26:45 +0300

    Fix abstract main Entity.java

--- src/main/java/ua/epam/akoreshev/finalproject/model/entity/Entity.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/entity/Entity.java
@@ -4,8 +4,7 @@ package ua.epam.akoreshev.finalproject.model.entity;
  * Basic class for every model entity
  */
 public abstract class Entity {
-
-    private long id;
+    protected long id;
 
     public long getId() {
         return id;
@@ -13,18 +12,5 @@ public abstract class Entity {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Entity entity = (Entity) o;
-        return id == entity.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Long.hashCode(id);
     }
 }
