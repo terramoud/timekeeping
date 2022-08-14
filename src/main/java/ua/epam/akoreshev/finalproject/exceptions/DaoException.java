1a68c03864999381424fa1739349e42651bc3f08
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-14 18:30:54 +0300

    Create DaoException.java

---
+++ src/main/java/ua/epam/akoreshev/finalproject/exceptions/DaoException.java
@@ -0,0 +1,16 @@
+package ua.epam.akoreshev.finalproject.exceptions;
+
+
+public class DaoException extends Exception {
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException() {
+        super();
+    }
+}
