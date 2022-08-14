02797733bc67958474280536857c85e62b2c6ef6
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-14 18:30:25 +0300

    Implement interface BaseDao.java

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/BaseDao.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/BaseDao.java
@@ -1,4 +1,22 @@
 package ua.epam.akoreshev.finalproject.model.dao;
 
-public interface BaseDao {
+
+import ua.epam.akoreshev.finalproject.exceptions.DaoException;
+import ua.epam.akoreshev.finalproject.model.entity.Entity;
+
+
+import java.util.List;
+
+public interface BaseDao<T extends Entity, ID> {
+    // List<?> findByPage(T entity, Pagination pagination, int offset, int limit);
+    List<T> findAll() throws DaoException;
+
+    boolean create(T t) throws DaoException;
+
+    T read(ID id) throws DaoException;
+
+    boolean update(T t) throws DaoException;
+
+    boolean delete(ID id) throws DaoException;
+
 }
