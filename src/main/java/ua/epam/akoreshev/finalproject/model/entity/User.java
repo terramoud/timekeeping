eec596c6fb0a9672c83f39cfb85141a48181788f
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-14 17:54:39 +0300

    Create and implement entity User

--- src/main/java/ua/epam/akoreshev/finalproject/model/entity/User.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/entity/User.java
@@ -1,4 +1,85 @@
 package ua.epam.akoreshev.finalproject.model.entity;
 
-public class User {
+import java.util.Objects;
+
+/**
+ * Represents relevant entity from database's table
+ * The User class instance equals to another User class
+ * instance only when they have same email and login and id
+ *
+ * @author Alexander Koreshev
+ */
+public class User extends Entity {
+    private String login;
+    private String email;
+    private String password;
+    private int roleId;
+
+    public User(long id, String login, String email, String password, int roleId) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    public User() {
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(login, user.login) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, login, email, password, roleId);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roleId=" + roleId +
+                '}';
+    }
 }
