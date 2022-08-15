78270092310c5fa45504bf52410bb290465eaac7
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-15 18:55:30 +0300

    Remove extra methods from ActivityDao interface

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/ActivityDao.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/ActivityDao.java
@@ -1,15 +1,14 @@
 package ua.epam.akoreshev.finalproject.model.dao;
 
+import ua.epam.akoreshev.finalproject.exceptions.DaoException;
 import ua.epam.akoreshev.finalproject.model.entity.Activity;
 
 import java.util.List;
 
 public interface ActivityDao extends BaseDao<Activity, Long> {
-    List<Activity> findAllActivitiesByCategory(String categoryName);
-    List<Activity> findAllActivitiesByCategory(long categoryId);
-    List<Activity> findAllActivitiesByUser(String login);
-    List<Activity> findAllActivitiesByUser(long userId);
-    boolean removeActivityByUser(String login);
-    boolean removeActivityByUser(long id);
+    List<Activity> findAllActivitiesByCategory(String categoryName) throws DaoException;
+    List<Activity> findAllActivitiesByCategory(long categoryId) throws DaoException;
+    List<Activity> findAllActivitiesByUser(String login) throws DaoException;
+    List<Activity> findAllActivitiesByUser(long userId) throws DaoException;
 }
 
