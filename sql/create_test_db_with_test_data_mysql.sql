a8a3abfa00aab305177a903e6d6ca4a855b95e56
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-15 12:53:33 +0300

    Fix database and create new schema
    Remove table translations and create instead of them additional localizing fields for every necessary table

--- sql/create_test_db_with_test_data_mysql.sql
+++ sql/create_test_db_with_test_data_mysql.sql
@@ -36,42 +36,14 @@ CREATE TABLE IF NOT EXISTS users
 
 
 -- -----------------------------------------------------
--- Table languages
--- -----------------------------------------------------
--- CREATE TABLE IF NOT EXISTS languages
--- (
---     PRIMARY KEY (id),
---     id   INT         NOT NULL AUTO_INCREMENT,
---     name VARCHAR(32) NOT NULL,
---     UNIQUE (name)
--- );
-
-
--- -----------------------------------------------------
--- Table translations
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS translations
-(
-    PRIMARY KEY (id),
-    id            INT         NOT NULL AUTO_INCREMENT,
-    translated_en VARCHAR(32) NOT NULL,
-    translated_uk VARCHAR(32) NOT NULL
-);
-
-
--- -----------------------------------------------------
 -- Table categories
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS categories
 (
     PRIMARY KEY (id),
-    id             INT         NOT NULL AUTO_INCREMENT,
-    name           VARCHAR(32) NOT NULL UNIQUE,
-    translation_id INT         NOT NULL UNIQUE,
-    FOREIGN KEY (translation_id)
-        REFERENCES translations (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    id      INT         NOT NULL AUTO_INCREMENT,
+    name_en VARCHAR(32) NOT NULL UNIQUE,
+    name_uk VARCHAR(32) NOT NULL UNIQUE
 );
 
 
@@ -81,16 +53,12 @@ CREATE TABLE IF NOT EXISTS categories
 CREATE TABLE IF NOT EXISTS activities
 (
     PRIMARY KEY (id),
-    id             INT         NOT NULL AUTO_INCREMENT,
-    name           VARCHAR(32) NOT NULL UNIQUE,
-    translation_id INT         NOT NULL UNIQUE,
-    category_id    INT         NOT NULL,
+    id          INT         NOT NULL AUTO_INCREMENT,
+    name_en     VARCHAR(32) NOT NULL UNIQUE,
+    name_uk     VARCHAR(32) NOT NULL UNIQUE,
+    category_id INT         NOT NULL,
     FOREIGN KEY (category_id)
         REFERENCES categories (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    FOREIGN KEY (translation_id)
-        REFERENCES translations (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
@@ -102,13 +70,9 @@ CREATE TABLE IF NOT EXISTS activities
 CREATE TABLE IF NOT EXISTS statuses
 (
     PRIMARY KEY (id),
-    id             INT         NOT NULL AUTO_INCREMENT,
-    name           VARCHAR(32) NOT NULL UNIQUE,
-    translation_id INT         NOT NULL UNIQUE,
-    FOREIGN KEY (translation_id)
-        REFERENCES translations (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    id      INT         NOT NULL AUTO_INCREMENT,
+    name_en VARCHAR(32) NOT NULL UNIQUE,
+    name_uk VARCHAR(32) NOT NULL UNIQUE
 );
 
 
@@ -118,13 +82,9 @@ CREATE TABLE IF NOT EXISTS statuses
 CREATE TABLE IF NOT EXISTS types
 (
     PRIMARY KEY (id),
-    id             INT         NOT NULL AUTO_INCREMENT,
-    name           VARCHAR(32) NOT NULL UNIQUE,
-    translation_id INT         NOT NULL UNIQUE,
-    FOREIGN KEY (translation_id)
-        REFERENCES translations (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    id      INT         NOT NULL AUTO_INCREMENT,
+    name_en VARCHAR(32) NOT NULL UNIQUE,
+    name_uk VARCHAR(32) NOT NULL UNIQUE
 );
 
 
@@ -221,57 +181,30 @@ VALUES (DEFAULT, 'ivan', 'testIvan@test.com', '3333', 2);
 -- -----------------------------------------------------
 -- fill statuses
 -- -----------------------------------------------------
-INSERT INTO translations
-VALUES (DEFAULT, 'Pending...', 'В очікуванні...');
-INSERT INTO statuses (id, name, translation_id)
-VALUES (DEFAULT, 'Pending...', LAST_INSERT_ID());
-INSERT INTO translations
-VALUES (DEFAULT, 'Approved', 'Одобрено');
-INSERT INTO statuses (id, name, translation_id)
-VALUES (DEFAULT, 'Approved', LAST_INSERT_ID());
-INSERT INTO translations
-VALUES (DEFAULT, 'Declined', 'Відхилено');
-INSERT INTO statuses (id, name, translation_id)
-VALUES (DEFAULT, 'Declined', LAST_INSERT_ID());
+INSERT INTO statuses VALUES (DEFAULT, 'Pending...', 'В очікуванні...');
+INSERT INTO statuses VALUES (DEFAULT, 'Approved', 'Одобрено');
+INSERT INTO statuses VALUES (DEFAULT, 'Declined', 'Відхилено');
 
 
 -- -----------------------------------------------------
 -- fill types
 -- -----------------------------------------------------
-INSERT INTO translations
-VALUES (DEFAULT, 'Add', 'Додати');
-INSERT INTO types
-VALUES (DEFAULT, 'default_type_name', LAST_INSERT_ID());
-INSERT INTO translations
-VALUES (DEFAULT, 'Remove', 'Видалити');
-INSERT INTO types
-VALUES (DEFAULT, 'default_type_name2', LAST_INSERT_ID());
+INSERT INTO types VALUES (DEFAULT, 'Add', 'Додати');
+INSERT INTO types VALUES (DEFAULT, 'Remove', 'Видалити');
 
 
 -- -----------------------------------------------------
 -- fill categories of activities
 -- -----------------------------------------------------
-INSERT INTO translations
-VALUES (DEFAULT, 'sport', 'спорт');
-INSERT INTO categories
-VALUES (DEFAULT, 'default_category_name', LAST_INSERT_ID());
-INSERT INTO translations
-VALUES (DEFAULT, 'Art', 'Мистецтво');
-INSERT INTO categories
-VALUES (DEFAULT, 'default_category_name2', LAST_INSERT_ID());
+INSERT INTO categories VALUES (DEFAULT, 'Sport', 'Cпорт');
+INSERT INTO categories VALUES (DEFAULT, 'Art', 'Мистецтво');
 
 
 -- -----------------------------------------------------
 -- fill activities
 -- -----------------------------------------------------
-INSERT INTO translations
-VALUES (DEFAULT, 'Swimming', 'Плавання');
-INSERT INTO activities
-VALUES (DEFAULT, 'default_activity_name', LAST_INSERT_ID(), 1);
-INSERT INTO translations
-VALUES (DEFAULT, 'Drawing', 'Малювання');
-INSERT INTO activities
-VALUES (DEFAULT, 'default_activity_name2', LAST_INSERT_ID(), 2);
+INSERT INTO activities VALUES (DEFAULT, 'Swimming', 'Плавання', 1);
+INSERT INTO activities VALUES (DEFAULT, 'Drawing', 'Малювання', 2);
 
 
 -- -----------------------------------------------------
@@ -286,18 +219,15 @@ VALUES (DEFAULT, 1, 2, 1, 2);
 -- -----------------------------------------------------
 -- fill users_activities
 -- -----------------------------------------------------
-INSERT INTO users_activities
-VALUES (1, 1, TRUE);
-INSERT INTO users_activities
-VALUES (1, 2, FALSE);
-INSERT INTO users_activities
-VALUES (2, 1, FALSE);
+INSERT INTO users_activities VALUES (1, 1, TRUE);
+INSERT INTO users_activities VALUES (1, 2, FALSE);
+INSERT INTO users_activities VALUES (2, 1, FALSE);
 
 
 -- -----------------------------------------------------
 -- fill intervals
 -- -----------------------------------------------------
 INSERT INTO intervals
-VALUES (DEFAULT, CURRENT_TIMESTAMP, TIMESTAMP('2022-09-29 17:54:04'), 1, 1);
+    VALUES (DEFAULT, CURRENT_TIMESTAMP, TIMESTAMP('2022-09-29 17:54:04'), 1, 1);
 INSERT INTO intervals
-VALUES (DEFAULT, TIMESTAMP('2022-09-29 17:54:04'), TIMESTAMP('2022-09-29 18:54:04'), 1, 2);
+    VALUES (DEFAULT, TIMESTAMP('2022-09-29 18:54:04'), TIMESTAMP('2022-09-29 18:54:04'), 1, 2);
