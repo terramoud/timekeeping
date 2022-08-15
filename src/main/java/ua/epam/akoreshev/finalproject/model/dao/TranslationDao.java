96bd4e6d8d541ff7145ff35345b575231d0337f7
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-15 09:16:51 +0300

    Create TranslationDao

---
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/TranslationDao.java
@@ -0,0 +1,6 @@
+package ua.epam.akoreshev.finalproject.model.dao;
+
+import ua.epam.akoreshev.finalproject.model.entity.Translation;
+
+public interface TranslationDao extends BaseDao<Translation, Long>{
+}
