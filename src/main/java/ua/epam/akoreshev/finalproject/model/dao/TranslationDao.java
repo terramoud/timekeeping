1e6ff534695e39b7ca1251ad326f7763686eb783
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-17 15:59:16 +0300

    Remove Translation.java and TranslationDao.java

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/TranslationDao.java
+++
@@ -1,6 +0,0 @@
-package ua.epam.akoreshev.finalproject.model.dao;
-
-import ua.epam.akoreshev.finalproject.model.entity.Translation;
-
-public interface TranslationDao extends BaseDao<Translation, Long>{
-}
