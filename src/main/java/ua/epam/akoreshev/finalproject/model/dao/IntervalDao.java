aea6994bfb64aa09aff9261ac68b4572baab48e5
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-17 12:05:35 +0300

    Fix IntervalDao arguments of methods

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/IntervalDao.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/IntervalDao.java
@@ -1,12 +1,16 @@
 package ua.epam.akoreshev.finalproject.model.dao;
 
+import ua.epam.akoreshev.finalproject.exceptions.DaoException;
 import ua.epam.akoreshev.finalproject.model.entity.Interval;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IntervalDao extends BaseDao<Interval, Long> {
-    boolean setStartTimeForUserAndActivity();
-    boolean setFinishTimeForUserAndActivity();
-    List<Interval> findAllIntervalsByUserAndActivity();
+    boolean setStartTimeForUserActivity(long userId, long activityId, LocalDateTime startTime) throws DaoException;
+
+    boolean setFinishTimeForUserActivity(long userId, long activityId, LocalDateTime finishTime) throws DaoException;
+
+    List<Interval> findAllIntervalsByUserActivity(long userId, long activityId) throws DaoException;
 }
 
