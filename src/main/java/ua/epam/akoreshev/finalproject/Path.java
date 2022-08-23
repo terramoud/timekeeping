c214fc7a57b58302dee17229c1ab65285091059d
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-23 15:34:09 +0300

    Add jsp pages paths

--- src/main/java/ua/epam/akoreshev/finalproject/Path.java
+++ src/main/java/ua/epam/akoreshev/finalproject/Path.java
@@ -1,4 +1,18 @@
 package ua.epam.akoreshev.finalproject;
 
-public class Path {
+public final class Path {
+    public static final String ERROR_PAGE404 = "/WEB-INF/jsp/error404.jsp";
+    public static final String ERROR_PAGE500 = "/WEB-INF/jsp/error500.jsp";
+    public static final String USER_PAGE = "/WEB-INF/jsp/user/user_page.jsp";
+    public static final String ADMIN_PAGE = "/WEB-INF/jsp/admin/admin_dashboard.jsp";
+    public static final String LIST_ACTIVITIES_PAGE = "/WEB-INF/jsp/admin/list_activities.jsp";
+    public static final String LIST_CATEGORIES_PAGE = "/WEB-INF/jsp/admin/list_categories.jsp";
+    public static final String LIST_USERS_PAGE = "/WEB-INF/jsp/admin/list_users.jsp";
+    public static final String REPORT_PAGE = "/WEB-INF/jsp/admin/timekeeping_report.jsp";
+    public static final String PROFILE_PAGE = "/WEB-INF/jsp/profile.jsp";
+    public static final String INDEX_PAGE = "/index.jsp";
+
+
+    public static final String LOGIN_COMMAND = "/controller?command=login";
+    public static final String LOGOUT_COMMAND = "/controller?command=logout";
 }
