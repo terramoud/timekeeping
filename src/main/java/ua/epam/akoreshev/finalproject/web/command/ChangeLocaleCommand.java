84a279b398f6e039d7c03422a5001ccabe009b84
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-26 15:58:15 +0300

    Implement ChangeLocaleCommand

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/ChangeLocaleCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/ChangeLocaleCommand.java
@@ -7,8 +7,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class ChangeLocaleCommand extends Command {
     private static final Logger LOG = LogManager.getLogger(ChangeLocaleCommand.class);
@@ -16,24 +14,12 @@ public class ChangeLocaleCommand extends Command {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
         String locale = req.getParameter("language");
-        LOG.debug("App locale is: {}", locale);
-
+        LOG.debug("Set locale to: {}", locale);
         HttpSession session = req.getSession();
         session.setAttribute("language", locale);
-        boolean isLocaleCookieExists = Arrays.stream(req.getCookies())
-                .anyMatch(e -> e.getName().equals("defaultLocale"));
-        if (!isLocaleCookieExists)
-            resp.addCookie(new Cookie("defaultLocale", locale));
-
-        String sourcePage = req.getParameter("source_page");
-        String redirectUrl = req.getParameterMap()
-                .entrySet().stream()
-                .filter(param -> !param.getKey().equals("source_page"))
-                .filter(param -> !param.getKey().equals("command"))
-                .map(e -> e.getKey().concat("=").concat(e.getValue()[0]))
-                .collect(Collectors.joining("&", "?","&"))
-                .concat("command=").concat(sourcePage);
-        LOG.debug("Redirect url is: {}", redirectUrl);
-        return redirectUrl;
+        LOG.debug("Sesion.setAttribute language is: {}", locale);
+        resp.addCookie(new Cookie("defaultLocale", locale));
+        LOG.debug("Cookie.setAttribute language is: {}", locale);
+        return req.getHeader("referer");
     }
 }
