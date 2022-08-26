efb2ec960f361b0ca64536b60455a50cc24dff32
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-26 16:08:50 +0300

    Set main Entity to serializable

--- src/main/java/ua/epam/akoreshev/finalproject/model/entity/Entity.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/entity/Entity.java
@@ -1,9 +1,12 @@
 package ua.epam.akoreshev.finalproject.model.entity;
 
+import java.io.Serializable;
+
 /**
  * Basic class for every model entity
  */
-public abstract class Entity {
+public abstract class Entity implements Serializable {
+    private static final long serialVersionUID = -1491456600908233143L;
     protected long id;
 
     public long getId() {
