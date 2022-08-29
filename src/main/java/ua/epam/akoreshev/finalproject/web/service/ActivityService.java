c75b0af5db73e18b5213d1f3960c51bbd540246f
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-29 15:20:47 +0300

    Add method to ActivityService

--- src/main/java/ua/epam/akoreshev/finalproject/web/service/ActivityService.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/service/ActivityService.java
@@ -2,8 +2,10 @@ package ua.epam.akoreshev.finalproject.web.service;
 
 
 import ua.epam.akoreshev.finalproject.exceptions.DaoException;
+import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
 import ua.epam.akoreshev.finalproject.model.entity.Activity;
 import ua.epam.akoreshev.finalproject.model.entity.Category;
+import ua.epam.akoreshev.finalproject.model.entity.User;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +18,9 @@ public interface ActivityService {
      * @return list of all users
      * @throws DaoException if unable to retrieve information for certain reasons
      */
-    Map<Category, List<Activity>> findAllActivitiesByCategories() throws DaoException;
+    Map<Category, List<Activity>> findAllActivitiesByCategories() throws ServiceException;
+
+    List<Activity> findAllActivitiesByUser(User user) throws ServiceException;
 }
 
 
--- src/main/java/ua/epam/akoreshev/finalproject/web/service/impl/ActivityServiceImpl.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/service/impl/ActivityServiceImpl.java
@@ -1,18 +1,21 @@
 package ua.epam.akoreshev.finalproject.web.service.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.exceptions.DaoException;
+import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
 import ua.epam.akoreshev.finalproject.model.dao.ActivityDao;
 import ua.epam.akoreshev.finalproject.model.entity.Activity;
 import ua.epam.akoreshev.finalproject.model.entity.Category;
+import ua.epam.akoreshev.finalproject.model.entity.User;
 import ua.epam.akoreshev.finalproject.web.service.ActivityService;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ActivityServiceImpl implements ActivityService {
-
+    private static final Logger LOG = LogManager.getLogger(ActivityServiceImpl.class);
     private final ActivityDao activityDao;
 
     public ActivityServiceImpl(ActivityDao activityDao) {
@@ -20,19 +23,33 @@ public class ActivityServiceImpl implements ActivityService {
     }
 
     /**
-     *
-     *
      * @return
      * @throws DaoException if unable to retrieve information for certain reasons
      */
     @Override
-    public Map<Category, List<Activity>> findAllActivitiesByCategories() throws DaoException {
-        List<Category> categories = activityDao.findAllCategories();
-        Map<Category, List<Activity>> categoryListMap = new LinkedHashMap<>();
-        for (Category category : categories) {
-            categoryListMap.put(category,
-                    activityDao.findAllActivitiesByCategory(category.getId()));
+    public Map<Category, List<Activity>> findAllActivitiesByCategories() throws ServiceException {
+        List<Category> categories = null;
+        try {
+            categories = activityDao.findAllCategories();
+            Map<Category, List<Activity>> categoryListMap = new LinkedHashMap<>();
+            for (Category category : categories) {
+                categoryListMap.put(category,
+                        activityDao.findAllActivitiesByCategory(category.getId()));
+            }
+            return categoryListMap;
+        } catch (DaoException e) {
+            LOG.error(e);
+            throw new ServiceException("Cannot find activities by categories " + e.getMessage());
         }
-        return categoryListMap;
+    }
+
+    @Override
+    public List<Activity> findAllActivitiesByUser(User user) throws ServiceException {
+        try {
+            return activityDao.findAllActivitiesByUser(user.getId());
+        } catch (DaoException e) {
+            LOG.error(e);
+            throw new ServiceException("Cannot find activities by user " + e.getMessage());
+        }
     }
 }
