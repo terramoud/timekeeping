80bde88f33461e3ccdd43b40ce50cb2829e8d3a6
Alexander Koreshev <a.koreshev1@gmail.com>
2022-08-29 15:10:07 +0300

    Add default methods for realize transaction in dao classes

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/BaseDao.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/BaseDao.java
@@ -5,6 +5,8 @@ import ua.epam.akoreshev.finalproject.exceptions.DaoException;
 import ua.epam.akoreshev.finalproject.model.entity.Entity;
 
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface BaseDao<T extends Entity, ID> {
@@ -18,5 +20,21 @@ public interface BaseDao<T extends Entity, ID> {
     boolean update(T t) throws DaoException;
 
     boolean delete(ID id) throws DaoException;
+
+    default void setAutoCommit(Connection connection) throws DaoException {
+        try {
+            connection.setAutoCommit(true);
+        } catch (SQLException throwables) {
+            throw new DaoException("Cannot finish transaction");
+        }
+    }
+
+    default void rollback(Connection connection) throws DaoException {
+        try {
+            connection.rollback();
+        } catch (SQLException throwables) {
+            throw new DaoException("Cannot rollback changes in db!");
+        }
+    }
 
 }
