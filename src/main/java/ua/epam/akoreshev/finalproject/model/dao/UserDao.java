aa170035bf844a8b417778f6d0034c99c7910e0a
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:46:47 +0300

    Remove extra methods

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/UserDao.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/UserDao.java
@@ -2,17 +2,14 @@ package ua.epam.akoreshev.finalproject.model.dao;
 
 import ua.epam.akoreshev.finalproject.exceptions.DaoException;
 import ua.epam.akoreshev.finalproject.model.entity.User;
-import ua.epam.akoreshev.finalproject.model.entity.UserActivityBean;
 
 import java.util.List;
 
 public interface UserDao extends BaseDao<User, Long> {
-    List<User> findAllUsersByActivity(long activityId) throws DaoException;
-    List<User> findAllUsersByActivity(String activityName) throws DaoException;
     User read(String login) throws DaoException;
 
     long getNumberUsers() throws DaoException;
 
-    List<User> findAllUsers(int limit, int offset) throws DaoException;
+    List<User> findAllUsers(int limit, int offset, String columnName, String sortOrder) throws DaoException;
 }
 
