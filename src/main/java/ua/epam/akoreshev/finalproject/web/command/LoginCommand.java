30717bcba77a87b6eb3ace79a9f446fb8fc187b6
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:02:02 +0300

    Add validator for request parameters and ability to return localized error messages

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/LoginCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/LoginCommand.java
@@ -5,13 +5,14 @@ import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.Path;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
+import ua.epam.akoreshev.finalproject.exceptions.UserException;
 import ua.epam.akoreshev.finalproject.model.entity.Role;
 import ua.epam.akoreshev.finalproject.model.entity.User;
 import ua.epam.akoreshev.finalproject.web.service.UserService;
+import ua.epam.akoreshev.finalproject.web.utils.RequestParameterValidator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
 public class LoginCommand extends Command {
@@ -26,24 +27,24 @@ public class LoginCommand extends Command {
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
         LOG.debug("Command starts");
         String redirectCommand = null;
-        String login = req.getParameter("login");
-        String password = req.getParameter("password");
+        RequestParameterValidator validator = new RequestParameterValidator(req);
+        String login = validator.getString("login");
+        String password = validator.getString("password");
         try {
             User user = userService.findUserByLoginAndPassword(login, password);
-            LOG.debug("User {} has logged in successfully", user);
-            HttpSession session = req.getSession();
-            session.setAttribute("user", user);
-            LOG.debug("Sesion.setAttribute user is: {}", user);
+            req.getSession().setAttribute("user", user);
+            LOG.debug("Session.setAttribute user is: {}", user);
             if (Objects.equals(Role.getRole(user.getRoleId()), Role.USER))
                 redirectCommand = Path.USER_PAGE_COMMAND;
-
             if (Objects.equals(Role.getRole(user.getRoleId()), Role.ADMIN))
                 redirectCommand = Path.ADMIN_PAGE_COMMAND;
-
             LOG.debug("Redirect to : {}", redirectCommand);
+        } catch (UserException e) {
+            putToSession(req, e.getMessage(), true, LOG);
+            redirectCommand = "?command=" + Path.INDEX_PAGE_COMMAND;
         } catch (ServiceException e) {
             LOG.error(e);
-            throw new CommandException("User hasn't access to app. " + e.getMessage());
+            throw new CommandException(e.getMessage(), e);
         }
         LOG.debug("Command finished");
         return redirectCommand;
