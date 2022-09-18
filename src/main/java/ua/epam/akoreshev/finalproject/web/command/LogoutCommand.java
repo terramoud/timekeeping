2666e733ac8eb8a3d2391db3c3b2524ac318d1e4
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:00:29 +0300

    Add logger

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/LogoutCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/LogoutCommand.java
@@ -1,5 +1,7 @@
 package ua.epam.akoreshev.finalproject.web.command;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.Path;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
 
@@ -7,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LogoutCommand extends Command {
+    private static final Logger LOG = LogManager.getLogger(LogoutCommand.class);
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
+        LOG.trace("Command starts");
         req.getSession().invalidate();
+        LOG.trace("Command finished");
         return Path.INDEX_PAGE;
     }
 }
