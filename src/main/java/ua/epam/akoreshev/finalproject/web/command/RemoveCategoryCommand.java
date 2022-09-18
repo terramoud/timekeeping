558270c30981998652dc193d0bc3ff2b987735e4
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:07:12 +0300

    Add request parameters validator and implement ability to return localized error messages

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/RemoveCategoryCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/RemoveCategoryCommand.java
@@ -4,29 +4,33 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
-import ua.epam.akoreshev.finalproject.web.service.ActivityService;
+import ua.epam.akoreshev.finalproject.web.service.CategoryService;
+import ua.epam.akoreshev.finalproject.web.utils.RequestParameterValidator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RemoveCategoryCommand extends Command {
     private static final Logger LOG = LogManager.getLogger(RemoveCategoryCommand.class);
-    private final ActivityService activityService;
-    public RemoveCategoryCommand(ActivityService activityService) {
-        this.activityService = activityService;
+    private final CategoryService categoryService;
+
+    public RemoveCategoryCommand(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
-        long categoryId = Long.parseLong(req.getParameter("category_id"));
-
+        LOG.trace("Command starts");
+        RequestParameterValidator validator = new RequestParameterValidator(req);
+        long categoryId = validator.getLong("category_id");
         try {
-            req.getSession().setAttribute("request_status", "failed");
-            if (activityService.removeCategory(categoryId)) {
-                req.getSession().setAttribute("request_status", "success");
+            putToSession(req, "category.remove.failed", true, LOG);
+            if (categoryService.removeCategory(categoryId)) {
+                putToSession(req, "category.remove.success", false, LOG);
             }
         } catch (ServiceException e) {
-            req.getSession().setAttribute("request_status", "failed");
+            LOG.error("Cannot remove category by id: {}", categoryId);
+            throw new CommandException("Cannot remove category by id", e);
         }
         LOG.trace("Command finished");
         return req.getHeader("referer");
