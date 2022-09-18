85c0ba72c32b60c8b8766d62afffb5ef89113b5e
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:17:20 +0300

    Rename method arguments to better readability

--- src/main/java/ua/epam/akoreshev/finalproject/web/service/RequestService.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/service/RequestService.java
@@ -1,5 +1,6 @@
 package ua.epam.akoreshev.finalproject.web.service;
 
+import ua.epam.akoreshev.finalproject.exceptions.RequestException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
 import ua.epam.akoreshev.finalproject.model.entity.Request;
 import ua.epam.akoreshev.finalproject.model.entity.UserActivityRequest;
@@ -7,13 +8,13 @@ import ua.epam.akoreshev.finalproject.model.entity.UserActivityRequest;
 import java.util.List;
 
 public interface RequestService {
-    boolean createRequest(Request request) throws ServiceException;
+    boolean createRequest(Request request) throws ServiceException, RequestException;
 
-    List<UserActivityRequest> findRequestsByStatuses(int limit, int offset, int[] statuses) throws ServiceException;
+    List<UserActivityRequest> findRequestsByStatuses(int limit, int offset, String column, String order, int[] statuses) throws ServiceException;
 
     int getCountRequestsByStatuses(int... statuses) throws ServiceException;
 
-    boolean approveRequest(long requestId, long userId, long activityId, long typeId) throws ServiceException;
+    boolean approveRequest(long requestId, long userId, long activityId, long typeId) throws ServiceException, RequestException;
 
-    boolean rejectRequest(long requestId) throws ServiceException;
+    boolean rejectRequest(long requestId) throws ServiceException, RequestException;
 }
