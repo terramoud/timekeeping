b792759441efba85143addf15d5789b34a3a0618
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:53:43 +0300

    Add editUser and changePassword methods

--- src/main/java/ua/epam/akoreshev/finalproject/web/service/UserService.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/service/UserService.java
@@ -1,21 +1,29 @@
 package ua.epam.akoreshev.finalproject.web.service;
 
 
-import ua.epam.akoreshev.finalproject.exceptions.DaoException;
+import ua.epam.akoreshev.finalproject.exceptions.EditUserException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
+import ua.epam.akoreshev.finalproject.exceptions.UserException;
 import ua.epam.akoreshev.finalproject.model.entity.User;
-import ua.epam.akoreshev.finalproject.model.entity.UserActivityBean;
 
 import java.util.List;
 
 public interface UserService {
-    boolean addUser(User user) throws ServiceException;
-    User findUserByLoginAndPassword(String login, String password) throws ServiceException;
+    boolean addUser(User user, String passwordConfirm) throws ServiceException, UserException;
+
+    User findUserByLoginAndPassword(String login, String password) throws ServiceException, UserException;
 
     long getNumberUsers() throws ServiceException;
 
-    List<User> getUsers(int limit, int offset) throws ServiceException;
+    List<User> getUsers(int limit, int offset, String columnName, String sortOrder) throws ServiceException;
 
     boolean removeUser(long userId) throws ServiceException;
+
+    boolean editUser(User user) throws ServiceException, EditUserException;
+
+    boolean changePassword(long userId,
+                           String oldPassword,
+                           String newPassword,
+                           String confirmedNewPassword) throws ServiceException, EditUserException;
 }
 
