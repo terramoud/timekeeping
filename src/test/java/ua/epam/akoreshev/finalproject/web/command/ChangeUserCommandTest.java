585216b4f7374d36d4e0e2e13771ccc3d7a0c274
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 23:03:11 +0300

    Create and implement tests for ChangeUserCommand

--- src/test/java/ua/epam/akoreshev/finalproject/web/command/ChangeUserCommandTest.java
+++ src/test/java/ua/epam/akoreshev/finalproject/web/command/ChangeUserCommandTest.java
@@ -1,43 +1,108 @@
-//package ua.epam.akoreshev.finalproject.web.command;
-//
-//import org.junit.jupiter.api.*;
-//import ua.epam.akoreshev.finalproject.exceptions.DaoException;
-//import ua.epam.akoreshev.finalproject.model.dao.DBUtil;
-//import ua.epam.akoreshev.finalproject.model.dao.impl.RequestDaoImpl;
-//import ua.epam.akoreshev.finalproject.web.service.UserService;
-//import ua.epam.akoreshev.finalproject.web.service.impl.RequestServiceImpl;
-//
-//import java.sql.Connection;
-//import java.sql.SQLException;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class ChangeUserCommandTest {
-//    private static ChangeUserCommand adminDashboardCommand;
-//    private static Connection connection;
-//
-//    @BeforeAll
-//    public static void setUpBeforeAll() throws DaoException {
-//        connection = DBUtil.getConnection();
-//        adminDashboardCommand = new ChangeUserCommand((UserService) new RequestServiceImpl(new RequestDaoImpl(connection)));
-//    }
-//
-//    @BeforeEach
-//    public void setUp() throws SQLException {
-//        connection.setAutoCommit(false);
-//    }
-//
-//    @AfterEach
-//    public void tearDown() throws SQLException {
-//        connection.rollback();
-//        connection.setAutoCommit(true);
-//    }
-//
-//    @AfterAll
-//    public static void tearDownBeforeAll() throws SQLException {
-//        connection.close();
-//    }
-//    @Test
-//    void execute() {
-//    }
-//}
+package ua.epam.akoreshev.finalproject.web.command;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.exceptions.EditUserException;
+import ua.epam.akoreshev.finalproject.exceptions.RequestException;
+import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
+import ua.epam.akoreshev.finalproject.model.entity.User;
+import ua.epam.akoreshev.finalproject.web.service.RequestService;
+import ua.epam.akoreshev.finalproject.web.service.UserService;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static ua.epam.akoreshev.finalproject.TestConstants.RESOURCE_BUNDLE_NAME;
+
+
+class ChangeUserCommandTest {
+    private Command command;
+    private HttpSession session;
+    private UserService userService;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        userService = Mockito.mock(UserService.class);
+        command = new ChangeUserCommand(userService);
+        req = Mockito.mock(HttpServletRequest.class);
+        resp = Mockito.mock(HttpServletResponse.class);
+        resp.setHeader(eq("referer"), any());
+        session = Mockito.mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getSession().getAttribute("user")).thenReturn(new User());
+        when(req.getAttribute(any())).thenReturn("");
+        ServletContext servletContext = Mockito.mock(ServletContext.class);
+        when(req.getServletContext()).thenReturn(servletContext);
+        when(servletContext.getInitParameter(any())).thenReturn(RESOURCE_BUNDLE_NAME);
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @AfterAll
+    public static void tearDownBeforeAll() {
+    }
+
+    /**
+     * @see ChangeUserCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrlAndPutToSessionSuccessMessage()
+            throws ServiceException, CommandException, EditUserException {
+        when(userService.editUser(any())).thenReturn(true);
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+        verify(session).setAttribute("isErrorMessage", false);
+        verify(session).setAttribute(eq("message"), any());
+    }
+
+    /**
+     * @see ChangeUserCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrlAndPutToSessionErrorMessage()
+            throws ServiceException, CommandException, EditUserException {
+        when(userService.editUser(any())).thenReturn(false);
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+        verify(session).setAttribute("isErrorMessage", true);
+        verify(session).setAttribute(eq("message"), any());
+    }
+
+    /**
+     * @see ChangeUserCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrlAndPutToSessionErrorMessageWhenServiceThrowRequestException()
+            throws ServiceException, CommandException, EditUserException {
+        when(userService.editUser(any())).thenThrow(new EditUserException("Cannot edit user"));
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+        verify(session).setAttribute("isErrorMessage", true);
+        verify(session).setAttribute(eq("message"), any());
+    }
+
+    /**
+     * @see ChangeUserCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldThrowExceptionWhenSessionOrUserIsNotExists()
+            throws ServiceException, EditUserException {
+        when(userService.editUser(any())).thenThrow(ServiceException.class);
+        assertThrows(CommandException.class, () -> command.execute(req, resp));
+    }
+}
