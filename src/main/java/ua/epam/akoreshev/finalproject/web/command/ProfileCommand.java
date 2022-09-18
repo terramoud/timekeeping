f4eb396e7aa782134dd7505a46bdd9de2cad349f
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:03:45 +0300

    Create and implement ProfileCommand

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/ProfileCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/ProfileCommand.java
@@ -1,15 +1,19 @@
 package ua.epam.akoreshev.finalproject.web.command;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.Path;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
-import ua.epam.akoreshev.finalproject.web.command.Command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ProfileCommand extends Command {
+    private static final Logger LOG = LogManager.getLogger(ProfileCommand.class);
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
+        LOG.trace("Command starts");
+        LOG.trace("Command finished");
         return Path.PROFILE_PAGE;
     }
 }
