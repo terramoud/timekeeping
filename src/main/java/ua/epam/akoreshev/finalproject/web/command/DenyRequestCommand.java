a30cf5276d008ce86213729a6d8ce44f0188ff7b
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 20:29:49 +0300

    Implement ability to return localized error messages

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/DenyRequestCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/DenyRequestCommand.java
@@ -3,9 +3,10 @@ package ua.epam.akoreshev.finalproject.web.command;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.exceptions.RequestException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
 import ua.epam.akoreshev.finalproject.web.service.RequestService;
-import ua.epam.akoreshev.finalproject.web.service.impl.RequestServiceImpl;
+import ua.epam.akoreshev.finalproject.web.utils.RequestParameterValidator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,21 +15,25 @@ public class DenyRequestCommand extends Command {
     private static final Logger LOG = LogManager.getLogger(DenyRequestCommand.class);
     private final RequestService requestService;
 
-    public DenyRequestCommand(RequestServiceImpl requestService) {
+    public DenyRequestCommand(RequestService requestService) {
         this.requestService = requestService;
     }
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
         LOG.trace("Command starts");
-        long requestId = Long.parseLong(req.getParameter("request_id"));
+        RequestParameterValidator validator = new RequestParameterValidator(req);
+        long requestId = validator.getLong("request_id");
         try {
-            req.getSession().setAttribute("request_status", "failed");
+            putToSession(req, "request.reject.failed", true, LOG);
             if (requestService.rejectRequest(requestId)) {
-                req.getSession().setAttribute("request_status", "success");
+                putToSession(req, "request.reject.success", false, LOG);
             }
+        } catch (RequestException e) {
+            putToSession(req, e.getMessage(), true, LOG);
         } catch (ServiceException e) {
-            throw new CommandException(e.getMessage());
+            LOG.error(e);
+            throw new CommandException(e.getMessage(), e);
         }
         LOG.trace("Command finished");
         return req.getHeader("referer");
