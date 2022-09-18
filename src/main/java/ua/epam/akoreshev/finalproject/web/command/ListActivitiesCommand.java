be399dad842b1711f4c273573ae259df61dc4b67
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 20:52:48 +0300

    Add ability to sort table and request parameters validator

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/ListActivitiesCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/ListActivitiesCommand.java
@@ -6,7 +6,9 @@ import ua.epam.akoreshev.finalproject.Path;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
 import ua.epam.akoreshev.finalproject.model.entity.ActivityCategoryBean;
+import ua.epam.akoreshev.finalproject.model.entity.Category;
 import ua.epam.akoreshev.finalproject.web.service.ActivityService;
+import ua.epam.akoreshev.finalproject.web.utils.RequestParameterValidator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +18,8 @@ public class ListActivitiesCommand extends Command {
     private static final Logger LOG = LogManager.getLogger(ListActivitiesCommand.class);
 
     private final ActivityService activityService;
-    private final int LIMIT = 5;
+    public static final String SORT_COLUMN = "order_by";
+    public static final String SORT_PARAMETER = "desc";
 
     public ListActivitiesCommand(ActivityService activityService) {
         this.activityService = activityService;
@@ -25,23 +28,33 @@ public class ListActivitiesCommand extends Command {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
         LOG.trace("Command starts");
-        boolean hasPagination = req.getParameter("pageNumber") != null;
-        int pageNumber = (hasPagination) ? Integer.parseInt(req.getParameter("pageNumber")) : 1;
+        RequestParameterValidator validator = new RequestParameterValidator(req);
+        String columnName = validator.getString(SORT_COLUMN).isEmpty()
+                ? "activities.name_en" : validator.getString(SORT_COLUMN);
+        String sortOrder = validator.getBoolean(SORT_PARAMETER) ? "DESC" : "ASC";
+        int pageNumber = validator.getPaginationPageNumber("pageNumber");
         try {
-            int totalPages = (int) Math.ceil(activityService.getNumberActivities() / (double) LIMIT);
+            int limit = 5;
+            int totalPages = (int) Math.ceil(activityService.getNumberActivities() / (double) limit);
             LOG.debug("ServletRequest.setAttribute totalPages is: {}", totalPages);
             req.setAttribute("totalPages", totalPages);
-
-            int offset = (pageNumber - 1) * LIMIT;
-            List<ActivityCategoryBean> activityCategoryBeans = activityService.getActivities(LIMIT, offset);
+            int offset = (pageNumber - 1) * limit;
+            List<ActivityCategoryBean> activityCategoryBeans = activityService.getActivities(limit, offset, columnName, sortOrder);
             LOG.debug("ServletRequest.setAttribute activityCategoryBeans is: {}", activityCategoryBeans);
             req.setAttribute("activityCategoryBeans", activityCategoryBeans);
+            List<Category> categories = activityService.findAllCategories();
+            LOG.debug("ServletRequest.setAttribute categories is: {}", categories);
+            req.setAttribute("categories", categories);
         } catch (ServiceException e) {
             LOG.error(e);
-            throw new CommandException(e.getMessage());
+            throw new CommandException(e.getMessage(), e);
         }
+        LOG.debug("ServletRequest.setAttribute order_by is: {}", columnName);
+        req.setAttribute(SORT_COLUMN, columnName);
         LOG.debug("ServletRequest.setAttribute pageNumber is: {}", pageNumber);
         req.setAttribute("pageNumber", pageNumber);
+        LOG.debug("ServletRequest.setAttribute desc is: {}", !validator.getBoolean(SORT_PARAMETER));
+        req.setAttribute(SORT_PARAMETER, !validator.getBoolean(SORT_PARAMETER));
         LOG.trace("Command finished");
         return Path.LIST_ACTIVITIES_PAGE;
     }
