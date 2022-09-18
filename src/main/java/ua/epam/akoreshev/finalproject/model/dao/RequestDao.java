bf7cc93d50eac69efba3c0f2656803393effec25
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 21:11:21 +0300

    Remove extra methods

--- src/main/java/ua/epam/akoreshev/finalproject/model/dao/RequestDao.java
+++ src/main/java/ua/epam/akoreshev/finalproject/model/dao/RequestDao.java
@@ -7,17 +7,11 @@ import ua.epam.akoreshev.finalproject.model.entity.UserActivityRequest;
 import java.util.List;
 
 public interface RequestDao extends BaseDao<Request, Long> {
-    List<Request> findAllRequestsByStatus(long statusId) throws DaoException;
-
-    List<Request> findAllRequestsByStatus(String statusName) throws DaoException;
-
     boolean updateRequestStatus(long requestId, long statusId) throws DaoException;
-
-    boolean updateRequestStatus(long requestId, String status) throws DaoException;
 
     Request read(long userId, long activityId, long typeId, long statusId) throws DaoException;
 
-    List<UserActivityRequest> findAllRequestsByStatuses(int limit, int offset, int[] statuses) throws DaoException;
+    List<UserActivityRequest> findAllRequestsByStatuses(int limit, int offset, String column, String order, int[] statuses) throws DaoException;
 
     int getCountRowsByStatuses(int... statuses) throws DaoException;
 
