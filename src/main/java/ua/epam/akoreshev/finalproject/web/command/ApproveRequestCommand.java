323af13e45507aac38a339342a93f7c6dc4005ca
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 19:47:31 +0300

    Fix bug that print error and success messages at same time

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/ApproveRequestCommand.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/ApproveRequestCommand.java
@@ -3,9 +3,10 @@ package ua.epam.akoreshev.finalproject.web.command;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.exceptions.RequestException;
 import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
 import ua.epam.akoreshev.finalproject.web.service.RequestService;
-import ua.epam.akoreshev.finalproject.web.service.impl.RequestServiceImpl;
+import ua.epam.akoreshev.finalproject.web.utils.RequestParameterValidator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,24 +15,27 @@ public class ApproveRequestCommand extends Command {
     private static final Logger LOG = LogManager.getLogger(ApproveRequestCommand.class);
     private final RequestService requestService;
 
-    public ApproveRequestCommand(RequestServiceImpl requestService) {
+    public ApproveRequestCommand(RequestService requestService) {
         this.requestService = requestService;
     }
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException {
         LOG.trace("Command starts");
-        long requestId = Long.parseLong(req.getParameter("request_id"));
-        long userId = Long.parseLong(req.getParameter("user_id"));
-        long activityId = Long.parseLong(req.getParameter("activity_id"));
-        long typeId = Long.parseLong(req.getParameter("type_id"));
+        RequestParameterValidator validator = new RequestParameterValidator(req);
+        long requestId = validator.getLong("request_id");
+        long userId = validator.getLong("user_id");
+        long activityId = validator.getLong("activity_id");
+        long typeId = validator.getLong("type_id");
         try {
-            req.getSession().setAttribute("request_status", "failed");
-            if (requestService.approveRequest(requestId, userId, activityId, typeId)) {
-                req.getSession().setAttribute("request_status", "success");
-            }
+            boolean isError = !requestService.approveRequest(requestId, userId, activityId, typeId);
+            String message = (isError) ? "request.approve.failed" : "request.approve.success";
+            putToSession(req, message, isError, LOG);
+        } catch (RequestException e) {
+            putToSession(req, e.getMessage(), true, LOG);
         } catch (ServiceException e) {
-            req.getSession().setAttribute("request_status", "failed");
+            LOG.error("Admin cannot approve request");
+            throw new CommandException("Admin cannot approve request", e);
         }
         LOG.trace("Command finished");
         return req.getHeader("referer");
