c5d145f6cde79f16756ae1c3c0ffa0b86f1c1fb5
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-18 20:22:44 +0300

    Add putToSession method for adding error messages

--- src/main/java/ua/epam/akoreshev/finalproject/web/command/Command.java
+++ src/main/java/ua/epam/akoreshev/finalproject/web/command/Command.java
@@ -1,7 +1,9 @@
 package ua.epam.akoreshev.finalproject.web.command;
 
 
+import org.apache.logging.log4j.Logger;
 import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.web.utils.LocalizedMessageBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,13 @@ public abstract class Command {
     }
 
     public abstract String execute(HttpServletRequest req, HttpServletResponse resp) throws CommandException;
+
+    public void putToSession(HttpServletRequest request, String message, boolean isError, Logger logger) {
+        LocalizedMessageBuilder lmb = new LocalizedMessageBuilder(request);
+        logger.debug("Session.setAttribute 'message': {}", lmb.getLocalizedMessage(message));
+        request.getSession().setAttribute("message", lmb.getLocalizedMessage(message));
+        request.getSession().setAttribute("isErrorMessage", isError);
+    }
 
     @Override
     public String toString() {
