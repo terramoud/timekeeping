83fb483451d0d2820d42041843570dee9c5195bb
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-20 20:51:00 +0300

    Create and implement tests

---
+++ src/test/java/ua/epam/akoreshev/finalproject/web/command/SetStopTimeCommandTest.java
@@ -0,0 +1,113 @@
+package ua.epam.akoreshev.finalproject.web.command;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.exceptions.IntervalException;
+import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
+import ua.epam.akoreshev.finalproject.model.entity.User;
+import ua.epam.akoreshev.finalproject.web.service.IntervalService;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static ua.epam.akoreshev.finalproject.TestConstants.RESOURCE_BUNDLE_NAME;
+
+class SetStopTimeCommandTest {
+    private Command command;
+    private HttpSession session;
+    private IntervalService intervalService;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        intervalService = Mockito.mock(IntervalService.class);
+        command = new SetStopTimeCommand(intervalService);
+        req = Mockito.mock(HttpServletRequest.class);
+        resp = Mockito.mock(HttpServletResponse.class);
+        resp.setHeader(eq("referer"), any());
+        session = Mockito.mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getSession().getAttribute("user")).thenReturn(new User());
+        when(req.getAttribute(any())).thenReturn("");
+        ServletContext servletContext = Mockito.mock(ServletContext.class);
+        when(req.getServletContext()).thenReturn(servletContext);
+        when(servletContext.getInitParameter(any())).thenReturn(RESOURCE_BUNDLE_NAME);
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @AfterAll
+    public static void tearDownBeforeAll() {
+    }
+
+    /**
+     * @see SetStopTimeCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrl()
+            throws ServiceException, CommandException, IntervalException {
+        when(intervalService.setFinishTimeForUserActivity(anyLong(), anyLong(), any())).thenReturn(true);
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+    }
+
+    /**
+     * @see SetStopTimeCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrlAndPutToSessionErrorMessage()
+            throws ServiceException, CommandException, IntervalException {
+        when(intervalService.setFinishTimeForUserActivity(anyLong(), anyLong(), any())).thenReturn(false);
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+        verify(session).setAttribute("isErrorMessage", true);
+        verify(session).setAttribute(eq("message"), any());
+    }
+
+    /**
+     * @see SetStopTimeCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrlAndPutToSessionErrorMessageWhenServiceThrowIntervalException()
+            throws ServiceException, CommandException, IntervalException {
+        when(intervalService.setFinishTimeForUserActivity(anyLong(), anyLong(), any()))
+                .thenThrow(new IntervalException("Some message"));
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+        verify(session).setAttribute("isErrorMessage", true);
+        verify(session).setAttribute(eq("message"), any());
+    }
+
+    /**
+     * @see SetStopTimeCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldThrowExceptionWhenServiceThrowException()
+            throws ServiceException, IntervalException {
+        when(intervalService.setFinishTimeForUserActivity(anyLong(), anyLong(), any())).thenThrow(ServiceException.class);
+        assertThrows(CommandException.class, () -> command.execute(req, resp));
+    }
+
+    /**
+     * @see SetStopTimeCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldThrowExceptionWhenSessionOrUserIsNotExists()
+            throws ServiceException, IntervalException {
+        Mockito.reset(session);
+        when(intervalService.setFinishTimeForUserActivity(anyLong(), anyLong(), any())).thenReturn(true);
+        assertThrows(NullPointerException.class, () -> command.execute(req, resp));
+    }
+}
