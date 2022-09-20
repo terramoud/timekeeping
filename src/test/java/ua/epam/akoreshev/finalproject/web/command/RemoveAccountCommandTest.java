2dd57d76b69d841ebbda87e617f4e010774cfc41
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-20 10:02:40 +0300

    Create and implement tests

---
+++ src/test/java/ua/epam/akoreshev/finalproject/web/command/RemoveAccountCommandTest.java
@@ -0,0 +1,91 @@
+package ua.epam.akoreshev.finalproject.web.command;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import ua.epam.akoreshev.finalproject.Path;
+import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.exceptions.RequestException;
+import ua.epam.akoreshev.finalproject.exceptions.ServiceException;
+import ua.epam.akoreshev.finalproject.model.entity.User;
+import ua.epam.akoreshev.finalproject.web.service.UserService;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static ua.epam.akoreshev.finalproject.TestConstants.RESOURCE_BUNDLE_NAME;
+
+class RemoveAccountCommandTest {
+    private Command command;
+    private HttpSession session;
+    private UserService userService;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        userService = Mockito.mock(UserService.class);
+        command = new RemoveAccountCommand(userService);
+        req = Mockito.mock(HttpServletRequest.class);
+        resp = Mockito.mock(HttpServletResponse.class);
+        resp.setHeader(eq("referer"), any());
+        session = Mockito.mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getSession().getAttribute("user")).thenReturn(new User());
+        when(req.getAttribute(any())).thenReturn("");
+        ServletContext servletContext = Mockito.mock(ServletContext.class);
+        when(req.getServletContext()).thenReturn(servletContext);
+        when(servletContext.getInitParameter(any())).thenReturn(RESOURCE_BUNDLE_NAME);
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @AfterAll
+    public static void tearDownBeforeAll() {
+    }
+
+    /**
+     * @see RemoveAccountCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnIndexPageUrl()
+            throws ServiceException, CommandException {
+        when(userService.removeUser(anyLong())).thenReturn(true);
+        assertEquals(Path.INDEX_PAGE, command.execute(req, resp));
+        verify(session).invalidate();
+    }
+
+    /**
+     * @see RemoveAccountCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnRefererAndPutToSessionErrorMessage()
+            throws ServiceException, CommandException {
+        when(userService.removeUser(anyLong())).thenReturn(false);
+        assertEquals(req.getHeader("referer"), command.execute(req, resp));
+        verify(session).setAttribute("isErrorMessage", true);
+        verify(session).setAttribute(eq("message"), any());
+    }
+
+    /**
+     * @see RemoveAccountCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldThrowExceptionWhenServiceThrowException()
+            throws ServiceException {
+        when(userService.removeUser(anyLong())).thenThrow(ServiceException.class);
+        assertThrows(CommandException.class, () -> command.execute(req, resp));
+    }
+}
