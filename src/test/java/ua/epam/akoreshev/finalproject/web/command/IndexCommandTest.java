4c2b18b9328011d08050fe0ac6057488229de845
Alexander Koreshev <a.koreshev1@gmail.com>
2022-09-20 20:43:14 +0300

    Create and implement tests

---
+++ src/test/java/ua/epam/akoreshev/finalproject/web/command/IndexCommandTest.java
@@ -0,0 +1,45 @@
+package ua.epam.akoreshev.finalproject.web.command;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import ua.epam.akoreshev.finalproject.Path;
+import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexCommandTest {
+    private Command command;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        command = new IndexCommand();
+        req = Mockito.mock(HttpServletRequest.class);
+        resp = Mockito.mock(HttpServletResponse.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @AfterAll
+    public static void tearDownBeforeAll() {
+    }
+
+    /**
+     * @see IndexCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrl() throws CommandException {
+        assertEquals(Path.INDEX_PAGE, command.execute(req, resp));
+    }
+}
---
+++ src/test/java/ua/epam/akoreshev/finalproject/web/command/LogoutCommandTest.java
@@ -0,0 +1,56 @@
+package ua.epam.akoreshev.finalproject.web.command;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import ua.epam.akoreshev.finalproject.Path;
+import ua.epam.akoreshev.finalproject.exceptions.CommandException;
+import ua.epam.akoreshev.finalproject.model.entity.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class LogoutCommandTest {
+    private Command command;
+    private HttpSession session;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        command = new LogoutCommand();
+        req = Mockito.mock(HttpServletRequest.class);
+        resp = Mockito.mock(HttpServletResponse.class);
+        session = Mockito.mock(HttpSession.class);
+        when(req.getSession()).thenReturn(session);
+        when(req.getSession().getAttribute("user")).thenReturn(new User());
+        when(req.getAttribute(any())).thenReturn("");
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @AfterAll
+    public static void tearDownBeforeAll() {
+    }
+
+    /**
+     * @see LogoutCommand#execute(HttpServletRequest, HttpServletResponse)
+     */
+    @Test
+    void testExecuteShouldReturnUrl() throws CommandException {
+        assertEquals(Path.INDEX_PAGE, command.execute(req, resp));
+        verify(session).invalidate();
+    }
+}
